1403e96f502f6ecf96f5ff8c518650232eaf6b1c
Kabilan Mohanraj <kabilanmohanraj@gmail.com>
2024-03-17 21:57:43 -0400

    Add: retry logic to accept the client

--- mini-redis/src/bin/server.rs
+++ mini-redis/src/bin/server.rs
@@ -1,21 +1,36 @@
 // server-side script
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::{tcp, TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 
 #[tokio::main]
 async fn main() {
     // listen to TCP connections
-    ini_listener().await;
+    init_listener().await;
 }
 
-async fn ini_listener() {
-    // create TCP listener
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+async fn init_listener() {
+    let mut retry_attempts = 5; // do not assume that the network is stable
 
-    // listen to connection
+    // listen to connection and retry
     loop {
-        match listener.accept().await {
-            Ok((_socket, addr)) => println!("new client: {:?}, information sent: {:?}", addr, _socket),
+        match TcpListener::bind("127.0.0.1:6379").await {
+            Ok(listener) => {
+                // binding done, listen to requests
+                match listener.accept().await {
+                    Ok((tcp_stream, socket_address)) => {
+                        println!("new client: {:?}, information received: {:?}", socket_address, tcp_stream);
+                    },
+                    Err(e) => {
+                        retry_attempts -= 1;
+                        if retry_attempts < 0 {
+                            println!("Exceeded maximum retry attempts... Exiting server...");
+                            return;
+                        }
+                        println!("couldn't get client: {:?}... Retrying...", e);
+                    },
+                }
+
+            },
             Err(e) => println!("couldn't get client: {:?}", e),
         }
     }
