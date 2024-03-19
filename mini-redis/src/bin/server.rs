c8a73f7e481b39edf67b51d5e5153d6f8d50db6b
Kabilan Mohanraj <kabilanmohanraj@gmail.com>
2024-03-19 03:21:31 -0400

    Add: TODO server

--- mini-redis/src/bin/server.rs
+++ mini-redis/src/bin/server.rs
@@ -1,11 +1,12 @@
-use std::fs::read;
-use std::sync::{Arc, RwLock};
 // server-side script
 use std::{time::Duration};
 use std::collections::HashMap;
-// use tokio::sync::{RwLock};
+use std::sync::{Arc, RwLock};
+use bytes::Bytes;
 use tokio::{net::{TcpListener, TcpStream}, time::sleep};
 use mini_redis::{Connection, Frame};
+
+type KVStore = Arc<RwLock<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
@@ -18,7 +19,8 @@ async fn init_listener() {
     // // shared state
     // the HashMap acts as our key-value store
     // let mut db: HashMap<String, Vec<u8>> = HashMap::new();
-    let mut db: Arc<RwLock<HashMap<String, Vec<u8>>>> = Arc::new(RwLock::new(HashMap::new()));
+    // TODO: Shard (using hash) the KVStore with Mutex/ RwLock wrapping
+    let mut db: KVStore = Arc::new(RwLock::new(HashMap::new()));
 
 
     let mut retry_attempts = 5; // we do not assume that the network is stable
@@ -62,7 +64,7 @@ async fn init_listener() {
     }
 }
 
-async fn process_query(tcp_stream: TcpStream, db: Arc<RwLock<HashMap<String, Vec<u8>>>>) -> () {
+async fn process_query(tcp_stream: TcpStream, db: KVStore) -> () {
     use mini_redis::Command::{self, Set, Get};
 
     // TCP connections managed by the `Connection` object from mini-redis
@@ -84,13 +86,13 @@ async fn process_query(tcp_stream: TcpStream, db: Arc<RwLock<HashMap<String, Vec
                     let response_frame = match cmd_temp {
                         Set(cmd) => {
                             let mut write_guard = db.write().unwrap();
-                            write_guard.insert(cmd.key().to_string(), cmd.value().to_vec()); // store the value as a byte array
+                            write_guard.insert(cmd.key().to_string(), cmd.value().clone()); // store the value as a byte array
                             Frame::Simple("SUCCESS".to_string())
                         }
                         Get(cmd) => {
                             let read_guard = db.read().unwrap();
                             if let Some(value) = read_guard.get(cmd.key()) {
-                                Frame::Bulk(value.clone().into())
+                                Frame::Bulk(value.clone())
                             } else {
                                 Frame::Null
                             }
