100e3eb6fa83885b6feffda28dc3100c998b9527
Kabilan Mohanraj <kabilanmohanraj@gmail.com>
2024-03-19 17:46:13 -0400

    Test: DB consistency across client requests

--- mini-redis/src/bin/client.rs
+++ mini-redis/src/bin/client.rs
@@ -9,6 +9,7 @@ async fn main() {
     let redis_addr: String = "localhost:6379".to_string();
 
     // create_client(redis_addr);
+    // TODO: retry logic for failed requests
     match client::connect(redis_addr).await {
         Ok(mut client) => {
             // client
@@ -17,13 +18,37 @@ async fn main() {
             match client.set("Hello", "World!".into()).await {
                 Ok(()) => println!("[SUCCESS] SET"),
                 Err(e) => println!("[FAIL] SET error: {:?}", e),
-            }
+            };
 
             match client.get("Hello").await {
                 Ok(Some(data)) => println!("[SUCCESS] GET output: {:?}", data),
                 Ok(None) => println!("[SUCCESS] GET output empty"),
                 Err(e) => println!("[FAIL] GET error: {:?}", e),
-            }
+            };
+
+            match client.set("Hello", "brother!".into()).await {
+                Ok(()) => println!("[SUCCESS] SET"),
+                Err(e) => println!("[FAIL] SET error: {:?}", e),
+            };
+
+            match client.get("Hello").await {
+                Ok(Some(data)) => println!("[SUCCESS] GET output: {:?}", data),
+                Ok(None) => println!("[SUCCESS] GET output empty"),
+                Err(e) => println!("[FAIL] GET error: {:?}", e),
+            };
+            
+            let mut client2 = client::connect("localhost:6379").await.unwrap();
+            match client2.set("Hello", "people".into()).await {
+                Ok(()) => println!("[SUCCESS] SET"),
+                Err(e) => println!("[FAIL] SET error: {:?}", e),
+            };
+
+            let mut client3 = client::connect("localhost:6379").await.unwrap();
+            match client3.get("Hello").await {
+                Ok(Some(data)) => println!("[SUCCESS] GET output: {:?}", data),
+                Ok(None) => println!("[SUCCESS] GET output empty"),
+                Err(e) => println!("[FAIL] GET error: {:?}", e),
+            };
         }
         Err(_) => panic!("[FAIL] Failed to establish connection"),
     };
