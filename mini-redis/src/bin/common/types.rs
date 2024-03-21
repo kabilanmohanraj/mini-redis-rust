178f125732efce9529f77151757b555f00e77330
Kabilan Mohanraj <kabilanmohanraj@gmail.com>
2024-03-20 22:13:31 -0400

    Add: common types for client server communication

---
+++ mini-redis/src/bin/common/types.rs
@@ -0,0 +1,18 @@
+use bytes::Bytes;
+use tokio::sync::oneshot;
+
+// represents operations such as SET, GET, PUBLISH etc.
+// to enable communication between the client and the clerk
+type OpResponse<T> = oneshot::Sender<mini_redis::Result<T>>;
+
+pub enum Op {
+    Set {
+        key: String,
+        value: Bytes,
+        reply_channel: OpResponse<()>
+    },
+    Get {
+        key: String,
+        reply_channel: OpResponse<Bytes>
+    },
+}
